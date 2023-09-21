59d13700517fd5506cb8126df2c6ccd4b3cbdfc0
ReetamBG <r33t4m@gmail.com>
2023-09-22 00:17:31 +0530

    Weather Icon Update Fixed

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q="
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
 
+const weatherIcon = document.querySelector(".weather-icon");
+
 async function checkWeather(city){
     const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
     var data = await response.json();
@@ -15,9 +17,23 @@ async function checkWeather(city){
     document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
     document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
     document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
-}
 
-checkWeather();
+    if(data.weather[0].main == "Clouds"){
+        weatherIcon.src = "./images/clouds.png";
+    }
+    if(data.weather[0].main == "Clear"){
+        weatherIcon.src = "./images/clear.png";
+    }
+    if(data.weather[0].main == "Rain"){
+        weatherIcon.src = "./images/rain.png";
+    }
+    if(data.weather[0].main == "Drizzle"){
+        weatherIcon.src = "./images/drizzle.png";
+    }
+    if(data.weather[0].main == "Mist"){
+        weatherIcon.src = "./images/mist.png";
+    }
+}
 
 searchBtn.addEventListener("click", ()=>{
     checkWeather(searchBox.value);
