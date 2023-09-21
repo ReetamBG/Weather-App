0dde53975a592f376682d1e7783c63cc18ae41cd
ReetamBG <r33t4m@gmail.com>
2023-09-22 00:00:30 +0530

    Added API

---
+++ script.js
@@ -0,0 +1,18 @@
+// c99c3a56066c5bd2733609e4792bf20e  -> api key 
+// https://api.openweathermap.org/data/2.5/weather?q=jorhat&appid=c99c3a56066c5bd2733609e4792bf20e&units=metric  -> api URL
+
+const apiKey = "c99c3a56066c5bd2733609e4792bf20e";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=jorhat";
+
+async function checkWeather(){
+    const response = await fetch(apiUrl + `&appid=${apiKey}`);
+    var data = await response.json();
+    // console.log(data);
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+}
+
+checkWeather();
+
